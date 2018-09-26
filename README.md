ad712d0f4797f5d4f05f738511fc4213009efbd6
tonyphan <phanxuanthinhbkpro@gmail.com>
2018-09-26 23:39:25 +0700

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# change_phone_number
-This is an app used to change all the number in phone automatically
--- app/src/main/java/com/tonyjstudio/chuyendoisodienthoai/MainActivity.java
+++ app/src/main/java/com/tonyjstudio/chuyendoisodienthoai/MainActivity.java
@@ -1,10 +1,20 @@
 package com.tonyjstudio.chuyendoisodienthoai;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.ContentProviderOperation;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -20,8 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Dialog mDialog;
     private TextView mAmountTextView;
     private ProgressBar mStatusProgressBar;
-    private final Map<String, String> mPhoneMap = new HashMap<String, String>()
-    {{
+    private final Map<String, String> mPhoneMap = new HashMap<String, String>() {{
         // viettel
         put("0162", "032");
         put("0163", "033");
@@ -86,45 +95,166 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mDialog.show();
 
-        doTransfer();
+        long id = getRawContactIdByName("tony", "phan");
+
+        ContentResolver contentResolver = getContentResolver();
+        updatePhoneNumber(contentResolver, id, ContactsContract.CommonDataKinds.Phone.TYPE_HOME, "0968252764");
+        //doTransfer();
     }
 
     /**
      * function to do the transfer task
      */
-    private void doTransfer() {
+//    private void doTransfer() {
+//
+//        if (mDialog.isShowing()) {
+//
+//            for (Map.Entry<String, String> entry : mPhoneMap.entrySet()) {
+//
+//                ArrayList<ContentProviderOperation> operationArrayList = new ArrayList<>();
+//                String where =
+//                        //ContactsContract.Data.DISPLAY_NAME + " = ? AND " +
+//                        ContactsContract.Data.MIMETYPE + " ? AND "
+//                                + String.valueOf(ContactsContract.CommonDataKinds.Phone.TYPE).substring(0, entry.getKey().length()) + " = ? ";
+//
+//                String[] params = new String[]{ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE, entry.getKey()};
+//
+//                operationArrayList.add(ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI)
+//                        .withSelection(where, params)
+//                        .withValue(ContactsContract.CommonDataKinds.Phone.DATA, entry.getValue())
+//                        .build());
+//
+//                try {
+//                    getContentResolver().applyBatch(ContactsContract.AUTHORITY, operationArrayList);
+//                } catch (Exception ex) {
+//                    ex.printStackTrace();
+//                }
+//            }
+//        }
+//    }
 
-        if (mDialog.isShowing()) {
+    /* Get raw contact id by contact given name and family name.
+     *  Return raw contact id.
+     * */
+    private long getRawContactIdByName(String givenName, String familyName) {
+        ContentResolver contentResolver = getContentResolver();
 
-            for (Map.Entry<String, String> entry : mPhoneMap.entrySet()) {
+        // Query raw_contacts table by display name field ( given_name family_name ) to get raw contact id.
 
-                ArrayList<ContentProviderOperation> operationArrayList = new ArrayList<>();
-                String where =
-                        //ContactsContract.Data.DISPLAY_NAME + " = ? AND " +
-                        ContactsContract.Data.MIMETYPE + " ? AND "
-                                + String.valueOf(ContactsContract.CommonDataKinds.Phone.TYPE).substring(0, entry.getKey().length()) + " = ? ";
+        // Create query column array.
+        String queryColumnArr[] = {ContactsContract.Contacts._ID,
+                ContactsContract.Contacts.LOOKUP_KEY,
+                ContactsContract.Contacts.DISPLAY_NAME_PRIMARY};
 
-                String[] params = new String[]{ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE, entry.getKey()};
+        // Create where condition clause.
+        String displayName = givenName + " " + familyName;
+        String whereClause = ContactsContract.RawContacts.DISPLAY_NAME_PRIMARY + " = '" + displayName + "'";
 
-                operationArrayList.add(ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI)
-                        .withSelection(where, params)
-                        .withValue(ContactsContract.CommonDataKinds.Phone.DATA, entry.getValue())
-                        .build());
+        // Query raw contact id through RawContacts uri.
+        Uri rawContactUri = ContactsContract.RawContacts.CONTENT_URI;
 
-                try {
-                    getContentResolver().applyBatch(ContactsContract.AUTHORITY, operationArrayList);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
+        // Return the query cursor.
+        Cursor cursor = null;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CONTACTS, android.Manifest.permission.WRITE_CONTACTS},
+                    1);
+        } else {
+            try {
+                cursor = contentResolver.query(rawContactUri, null, whereClause, null, null);
+            } catch (Exception ex) {
+                Log.e("ERRRRRRRROR", ex.toString());
             }
+
         }
+
+        long rawContactId = 0;
+
+        if (cursor != null) {
+            // Get contact count that has same display name, generally it should be one.
+            int queryResultCount = cursor.getCount();
+            // This check is used to avoid cursor index out of bounds exception. android.database.CursorIndexOutOfBoundsException
+            if (queryResultCount > 0) {
+                // Move to the first row in the result cursor.
+                cursor.moveToFirst();
+                // Get raw_contact_id.
+                String s = cursor.getString(cursor.getColumnIndex(ContactsContract.RawContacts.DISPLAY_NAME_PRIMARY));
+                Log.d("tag", s);
+                rawContactId = cursor.getLong(cursor.getColumnIndex(ContactsContract.RawContacts._ID));
+            }
+            cursor.close();
+        }
+
+        return rawContactId;
     }
 
-    /**
-     * function to get list contacts
+
+    /*
+     * Update contact phone number by contact name.
+     * Return update contact number, commonly there should has one contact be updated.
      */
-    private ArrayList<Contact> getListContact() {
-        ArrayList<ContentProviderOperation> operationArrayList = new ArrayList<>();
-        ContentProviderOperation.Builder builder = ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI);
+    private int updateContactPhoneByName(String givenName, String familyName) {
+        int ret = 0;
+
+        ContentResolver contentResolver = getContentResolver();
+
+        // Get raw contact id by display name.
+        long rawContactId = getRawContactIdByName(givenName, familyName);
+
+        // Update data table phone number use contact raw contact id.
+        if (rawContactId > -1) {
+            // Update mobile phone number.
+            updatePhoneNumber(contentResolver, rawContactId, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE, "66666666666666");
+
+            // Update work mobile phone number.
+            updatePhoneNumber(contentResolver, rawContactId, ContactsContract.CommonDataKinds.Phone.TYPE_WORK_MOBILE, "8888888888888888");
+
+            // Update home phone number.
+            updatePhoneNumber(contentResolver, rawContactId, ContactsContract.CommonDataKinds.Phone.TYPE_HOME, "99999999999999999");
+
+            ret = 1;
+        } else {
+            ret = 0;
+        }
+
+        return ret;
+    }
+
+    /* Update phone number with raw contact id and phone type.*/
+    private void updatePhoneNumber(ContentResolver contentResolver, long rawContactId, int phoneType, String newPhoneNumber) {
+        // Create content values object.
+        ContentValues contentValues = new ContentValues();
+
+        // Put new phone number value.
+        contentValues.put(ContactsContract.CommonDataKinds.Phone.NUMBER, newPhoneNumber);
+
+        // Create query condition, query with the raw contact id.
+        StringBuffer whereClauseBuf = new StringBuffer();
+
+        // Specify the update contact id.
+        whereClauseBuf.append(ContactsContract.Data.RAW_CONTACT_ID);
+        whereClauseBuf.append("=");
+        whereClauseBuf.append(rawContactId);
+
+        // Specify the row data mimetype to phone mimetype( vnd.android.cursor.item/phone_v2 )
+        whereClauseBuf.append(" and ");
+        whereClauseBuf.append(ContactsContract.Data.MIMETYPE);
+        whereClauseBuf.append(" = '");
+        String mimetype = ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE;
+        whereClauseBuf.append(mimetype);
+        whereClauseBuf.append("'");
+
+        // Specify phone type.
+        whereClauseBuf.append(" and ");
+        whereClauseBuf.append(ContactsContract.CommonDataKinds.Phone.TYPE);
+        whereClauseBuf.append(" = ");
+        whereClauseBuf.append(phoneType);
+
+        // Update phone info through Data uri.Otherwise it may throw java.lang.UnsupportedOperationException.
+        Uri dataUri = ContactsContract.Data.CONTENT_URI;
+
+        // Get update data count.
+        int updateCount = contentResolver.update(dataUri, contentValues, whereClauseBuf.toString(), null);
     }
 }
